6d84bb8fadb78982b29268c54b0d5717b0f8981f
Alberto De Caro <albertodecaroit@gmail.com>
2025-06-01 18:36:22 +0200

    [Task] Main Menu
    - Creation of the main menu and scene selection, audio and adapting Neolithic scene to fit the loading from menu.

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public void LoadSceneByIndex(int buildIndex)
+    {
+        SceneManager.LoadScene(buildIndex);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
